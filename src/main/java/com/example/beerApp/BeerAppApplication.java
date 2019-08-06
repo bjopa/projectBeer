5019f2f2fe8fe97a497303ed0a19c94d9f87f5cd
mikaelasoderberg <mikaelasoderberg@live.se>
2019-08-06 16:08:17 +0200

    Connected java-file with db

--- src/main/java/com/example/beerApp/BeerAppApplication.java
+++ src/main/java/com/example/beerApp/BeerAppApplication.java
@@ -3,11 +3,33 @@ package com.example.beerApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.*;
+
 @SpringBootApplication
 public class BeerAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BeerAppApplication.class, args);
-	}
+	public static void main(String[] args) throws SQLException {
 
+//		String connstr = "jdbc:sqlserver://localhost;databaseName=SkumMasters;user=skumadmin;password=123;";
+
+		String connstr = "jdbc:sqlserver://skummasters.database.windows.net:1433;database=SkumMaster;user=skumadmin@skummasters;password=Hej12345;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+
+		String sql = "SELECT * FROM BEER";
+
+		try (Connection conn = DriverManager.getConnection(connstr)) {
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+
+			if (rs.next()) {
+				System.out.println(rs.findColumn("Style"));
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
+
+
+
+
